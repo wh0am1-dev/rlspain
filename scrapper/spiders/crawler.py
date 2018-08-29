3a56778f716b6a012f88564a4143c78637eb46b5
Carlos Aguilar <carlos.aguilar.112358@gmail.com>
2018-08-29 20:10:09 +0200

    feat: spider extracting mmr

--- scrapper/spiders/crawler.py
+++ scrapper/spiders/crawler.py
@@ -8,6 +8,19 @@ from scrapy.http import Request
 class RLScrapper(scrapy.Spider):
     name = 'rlscrapper'
     base_url = 'https://rocketleague.tracker.network/profile/mmr/steam/'
+    start_urls = []
+
+    def __init__(self, category=None, *args, **kwargs):
+        super(RLScrapper, self).__init__(*args, **kwargs)
+
+        data = self.load_json()
+        if not data:
+            return []
+
+        urls = [data[user]['id'] for user in data]
+        for i, v in enumerate(urls):
+            urls[i] = self.base_url + str(urls[i])
+        self.start_urls = urls
 
     @staticmethod
     def load_json():
@@ -16,17 +29,5 @@ class RLScrapper(scrapy.Spider):
             data = json.load(f)
         return data
 
-    def start_requests(self):
-        data = self.load_json()
-        if not data:
-            return []
-
-        urls = [data[user]['id'] for user in data]
-        for i, v in enumerate(urls):
-            urls[i] = self.base_url + str(urls[i])
-
-        return [Request(url) for url in urls]
-
     def parse(self, response):
-        for mmr in response.css('a[data-id="13 > span"]'):
-            yield {'mmr': mmr.extract()}
+        yield {'mmr': response.css('a[data-id="13"] > span ::text').extract_first()}
