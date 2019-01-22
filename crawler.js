ee5abea3bc76a345c9a62eaca358ce5eaba87e9f
Neko250 <carlos.aguilar.112358@gmail.com>
2019-01-22 20:31:02 +0100

    fix: update mmrs before extracting

--- crawler.js
+++ crawler.js
@@ -1,26 +1,28 @@
 const fs = require('fs')
 const crawler = require('crawler')
 
-const URL = 'https://rocketleague.tracker.network/profile/mmr'
-const buildURL =  p => `${URL}/${p.ps ? 'ps' : 'steam'}/${p.id}`
-
-const readDB = () => JSON.parse(fs.readFileSync('assets/data/db.json', 'utf8'))
-const writeDB = db => fs.writeFile('assets/data/db.json', JSON.stringify(db, null, 2), () => process.exit())
-const idx = (db, id) => { for (let i = 0; i < db.length; i++) if (db[i].id == id) return i; return null }
-const updatePlayer = (db, id, mmr) => db[idx(db, id)].mmr = parseInt(mmr)
-const comparePlayers = (a, b) => (a.mmr < b.mmr ? 1 : (a.mmr > b.mmr ? -1 : 0))
-
-const crawl = () => {
+function crawl() {
   let db = readDB()
 
   let c = new crawler({
     maxConnections: 10,
     callback: (error, res, done) => {
-      if (error) return
+      if (error) {
+        console.log(error)
+        done()
+        return
+      }
 
+      // check if we are refreshing or obtaining the mmr
+      let uriParts = res.request.uri.path.split('/')
+      if (uriParts.indexOf('mmr') == -1) {
+        done()
+        return
+      }
+
+      // extract the mmr from the webpage
       let $ = res.$
-      let id = res.request.uri.path.split('/')
-      id = id[id.length - 1]
+      let id = uriParts[uriParts.length - 1]
       let mmr = $('.card-list-item[data-id="13"] > .badge').text()
 
       updatePlayer(db, id, mmr)
@@ -30,6 +32,39 @@ const crawl = () => {
 
   c.on('drain', () => writeDB(db.sort(comparePlayers)))
   db.forEach(player => c.queue(buildURL(player)))
+  db.forEach(player => c.queue(buildURL(player, true)))
+}
+
+function buildURL(player, mmr = false) {
+  const URL = 'https://rocketleague.tracker.network/profile'
+  return `${URL}${mmr ? '/mmr' : ''}/${player.ps ? 'ps' : 'steam'}/${player.id}`
+}
+
+function readDB() {
+  return JSON.parse(fs.readFileSync('assets/data/db.json', 'utf8'))
+}
+
+function writeDB(db) {
+  fs.writeFileSync('assets/data/db.json', JSON.stringify(db, null, 2))
+  process.exit()
+}
+
+function comparePlayers(a, b) {
+  return a.mmr < b.mmr ? 1 : (a.mmr > b.mmr ? -1 : 0)
+}
+
+function idx(db, id) {
+  for (let i = 0; i < db.length; i++)
+    if (db[i].id == id)
+      return i
+
+  return null
+}
+
+function updatePlayer(db, id, mmr) {
+  let i = idx(db, id)
+  db[i]._mmr = db[i].mmr
+  db[i].mmr = parseInt(mmr)
 }
 
 crawl()
