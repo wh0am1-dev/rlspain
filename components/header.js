f07198c86452b204224a2a045bee749aa549f19c
Neko250 <carlos.aguilar.112358@gmail.com>
2019-01-22 19:43:50 +0100

    style: add icon glow

--- components/header.js
+++ components/header.js
@@ -6,8 +6,8 @@ module.exports = () => {
   return html`
     <div class="tc vh-100 w-100 dt">
       <div class="dtc v-mid">
-        <img src="assets/img/rlscrapper.svg" class="w-50 w-third-m w-25-l">
-        <h1 class="f3 f2-m f1-l"><span class="bg-red near-black ph3">rlspain.cf</span></h1>
+        <img src="assets/img/rlscrapper.svg" class="w-50 w-third-m w-25-l bt b--red br-pill shadow-2">
+        <h1 class="f3 f2-m f1-l mt4"><span class="bg-red near-black ph3">rlspain.cf</span></h1>
       </div>
     </div>
   `
