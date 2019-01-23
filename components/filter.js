af2a246a22b01017149a37fdcb4a480305ae693e
Neko250 <carlos.aguilar.112358@gmail.com>
2019-01-23 17:44:31 +0100

    feat: link to contact form

--- components/filter.js
+++ components/filter.js
@@ -7,19 +7,19 @@ const button = require('./button')
 module.exports = (filter, emit) => {
   return html`
     <div class="center w-100 w-50-m w-third-l mb4 tc">
-      ${button('¡Añádeme a la lista!', register)}
+      ${button('Peticiones', openForm)}
 
-      <input class="bg-near-black near-white br-pill b--yellow pv2 ph3 w-100 mt4"
+      <input class="input-reset outline-0 bg-near-black near-white br-pill b--yellow pv2 ph3 w-100 mt4"
         value=${filter.query} oninput=${update}
         type="text" name="query" placeholder="Buscar...">
     </div>
   `
 
-  function update(e) {
+  function update (e) {
     emit('filter:update', e.target.value)
   }
 
-  function register() {
-    window.open('https://google.com', '_blank')
+  function openForm () {
+    window.open('https://goo.gl/forms/ue1I68eSmeDDpTvj2', '_blank')
   }
 }
