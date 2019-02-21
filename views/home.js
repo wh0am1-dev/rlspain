2688fe54ff05c301e32554c91950f96370778d22
Carlos Aguilar <Carlos.Aguilar@sequel.com>
2019-02-21 14:06:28 +0000

    feat: add maintenance view

--- views/home.js
+++ views/home.js
@@ -8,9 +8,11 @@ const filter = require('../components/filter')
 const list = require('../components/list/list')
 const link = require('../components/link')
 const heart = require('../components/heart')
+const maintenance = require('../components/maintenance')
 
 module.exports = (state, emit) => {
   if (state.title !== title) emit('DOMTitleChange', title)
+  const maintenanceMode = true
 
   return html`
     <body class="code lh-copy bg-near-black near-white">
@@ -18,8 +20,7 @@ module.exports = (state, emit) => {
         ${header()}
 
         <div id="content" class="min-vh-100 pt5">
-          ${filter(state.filter, emit)}
-          ${list(state.db, state.filter)}
+          ${body()}
 
           <footer class="ph4 mt4 cf tc">
             <p class="f6 f5-m f4-l">made with ${heart()} by<br>${link('neko250', 'https://github.com/neko250', true)} & ${link('Salve', 'https://twitter.com/Salve_RL', true)}</p>
@@ -28,4 +29,15 @@ module.exports = (state, emit) => {
       </main>
     </body>
   `
+
+  function body () {
+    if (maintenanceMode) {
+      return maintenance()
+    }
+
+    return html`
+      ${filter(state.filter, emit)}
+      ${list(state.db, state.filter)}
+    `
+  }
 }
