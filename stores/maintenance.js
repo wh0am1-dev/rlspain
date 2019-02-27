92017008750df8a8e2cdbf7b5329f0150b821721
Neko250 <carlos.aguilar.112358@gmail.com>
2019-02-27 00:48:43 +0000

    fix: add render to maintenance events

--- stores/maintenance.js
+++ stores/maintenance.js
@@ -7,13 +7,16 @@ module.exports = (state, emitter) => {
 
   emitter.on('maintenance:on', () => {
     state.maintenance = true
+    emitter.emit('render')
   })
 
   emitter.on('maintenance:off', () => {
     state.maintenance = false
+    emitter.emit('render')
   })
 
   emitter.on('maintenance:toggle', () => {
-    state.maintenance = state.maintenance
+    state.maintenance = !state.maintenance
+    emitter.emit('render')
   })
 }
