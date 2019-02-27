e37731b46071c80ff4f64a38d75202474a14ceed
Neko250 <carlos.aguilar.112358@gmail.com>
2019-02-28 00:32:53 +0100

    style: change header title style

--- components/header.js
+++ components/header.js
@@ -7,7 +7,7 @@ module.exports = () => {
     <div class="tc vh-100 w-100 dt">
       <div class="dtc v-mid">
         <img src="assets/img/rlscrapper.svg" onclick=${scroll} class="w-50 w-third-m w-25-l bt bl b--yellow bw2 br-pill shadow-5 grow">
-        <h1 class="f3 f2-m f1-l mt4"><span class="bg-yellow near-black ph3 shadow-5 br2">rlspain.cf</span></h1>
+        <h1 class="f3 f2-m f1-l mt4"><span class="bg-yellow near-black ph3 pv2 shadow-5 br2">rlspain.cf</span></h1>
       </div>
     </div>
   `
