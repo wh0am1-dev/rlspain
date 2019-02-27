9b6ce8ff2af13259d24d9613b8c5ee14b01a5440
Neko250 <carlos.aguilar.112358@gmail.com>
2019-02-28 00:32:23 +0100

    style: change button styles

--- components/button.js
+++ components/button.js
@@ -2,9 +2,9 @@
 
 const html = require('choo/html')
 
-module.exports = (text, handler) => {
+module.exports = (text, handler, classes = '') => {
   return html`
-    <button class="br-pill b--yellow dim ph3 pv2 pointer bg-near-black yellow outline-0" onclick=${handler}>
+    <button class="br-pill b--yellow ph3 pv2 pointer bg-near-black yellow hover-bg-yellow hover-near-black bg-animate outline-0 ${classes}" onclick=${handler}>
       ${text}
     </button>
   `
