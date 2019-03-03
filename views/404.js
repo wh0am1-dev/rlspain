475496acf2eee3a9645d8d335858a590e66f8f2b
Neko250 <carlos.aguilar.112358@gmail.com>
2019-03-03 22:34:58 +0100

    style: revamp 404 view

--- views/404.js
+++ views/404.js
@@ -9,11 +9,21 @@ module.exports = (state, emit) => {
   if (state.title !== title) emit('DOMTitleChange', title)
 
   return html`
-    <body class="code lh-copy bg-near-black near-white pa4" style="cursor: default; user-select: none;">
-      <h2>route not found...</h2>
-      <h2 class="mb5">(╯°□°)╯︵ ┻━┻</h2>
+    <body class="code lh-copy bg-near-black near-white" style="cursor: default; user-select: none; scroll-behavior: smooth;">
+      <main class="ph4 cf center">
+        <div class="tc w-100 dt">
+          <div class="dtc v-mid">
+            <h2 class="f3 f1-ns mt5"><span class="bg-yellow near-black ph3 pv2 shadow-5 br2">404</span></h2>
 
-      ${button('back to main', () => emit('pushState', '/'))}
+            <div class="mv5">
+              <h2>(╯°□°)╯︵ ┻━┻</h2>
+              <h1 class="f4 f3-m f2-l w-50 center">No hay nada aquí...</h1>
+            </div>
+
+            ${button('Inicio', () => emit('pushState', '/'))}
+          </div>
+        </div>
+      </main>
     </body>
   `
 }
